480b520263b01ef06bafa6026c42c410112dd1a3
Rob Cresswell <robert.cresswell@outlook.com>
2016-10-28 14:52:42 +0200

    Update XStatic-roboto to 0.5.0.0
    Change-Id: I649068f04d2a44ea1d429f1cd9e903ef7c52437f

--- xstatic/pkg/roboto_fontface/__init__.py
+++ xstatic/pkg/roboto_fontface/__init__.py
@@ -15,9 +15,9 @@ NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
                                # please use a all-lowercase valid python
                                # package name
 
-VERSION = '0.4.3' # version of the packaged files, please use the upstream
+VERSION = '0.5.0' # version of the packaged files, please use the upstream
                   # version number
-BUILD = '2' # our package build number, so we can release new builds
+BUILD = '0' # our package build number, so we can release new builds
              # with fixes for xstatic stuff.
 PACKAGE_VERSION = VERSION + '.' + BUILD # version used for PyPi
 
@@ -28,14 +28,14 @@ CLASSIFIERS = []
 KEYWORDS = 'roboto_fontface xstatic'
 
 # XStatic-* package maintainer:
-MAINTAINER = 'Diana Whitten'
-MAINTAINER_EMAIL = 'hurgleburgler@gmail.com'
+MAINTAINER = 'Rob Cresswell'
+MAINTAINER_EMAIL = 'robert.cresswell@outlook.com'
 
 # this refers to the project homepage of the stuff we packaged:
 HOMEPAGE = 'https://github.com/choffmeister/roboto-fontface-bower'
 
 # this refers to all files:
-LICENSE = '(same as %s)' % DISPLAY_NAME
+LICENSE = 'Apache-2.0'
 
 from os.path import join, dirname
 BASE_DIR = join(dirname(__file__), 'data')
--- xstatic/pkg/roboto_fontface/data/README.md
+++ xstatic/pkg/roboto_fontface/data/README.md
@@ -14,6 +14,26 @@ $ npm install roboto-fontface --save
 $ bower install roboto-fontface --save
 ```
 
+
+## Usage
+
+There're several files in the `css/` subdirectory. Import them in your project
+to have access to "Roboto" font face:
+
+* `css/roboto/roboto-fontface.css` - whole font family compiled to CSS
+* `css/roboto/sass/roboto-fontface.scss` - whole font family in SCSS
+* `css/roboto/less/roboto-fontface.less` - whole font family in LESS
+
+* `css/roboto-condensed/roboto-condensed-fontface.css` - whole font family compiled to CSS
+* `css/roboto-condensed/sass/roboto-condensed-fontface.scss` - whole font family in SCSS
+* `css/roboto-condensed/less/roboto-condensed-fontface.less` - whole font family in LESS
+
+Importing whole family may be unnecessary and lead to huge build, so if you are
+using SCSS or LESS, you can import only individual weights by importing for example:
+
+* `css/roboto/sass/roboto-fontface-black.scss`
+* `css/roboto/sass/roboto-fontface-black-italic.scss`
+
 ## Hinting
 
 Some of the included font files have [hinting](http://en.wikipedia.org/wiki/Font_hinting).
